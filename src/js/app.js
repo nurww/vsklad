efd93decd621526e2367c01e2a345ccaad423d30
nurww <nurdaule@gmail.com>
2020-05-28 08:24:04 +0600

    deploying landing

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// Import Fonts
+import '../fonts/montserrat.scss';
+
+// Import stylesheets
+import '../scss/css-reset.css';
+import '../scss/main.scss';
+import '../scss/header.scss';
+import '../scss/pageHeader.scss';
+import '../scss/content.scss';
+import '../scss/asideImg.scss';
+import '../scss/headerDropdown.scss';
+
+// Import img
+import '../img/Asset 25.png';
+import '../img/logo.svg';
+import '../img/asideImg.png';
+import '../img/menuIcon.svg';
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  context: path.resolve(__dirname, 'src'),
+  devServer: {
+    publicPath: '/',
+    port: 9000,
+    contentBase: path.join(process.cwd(), 'dist'),
+    host: 'localhost',
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true}
+          }
+        ]
+      },
+      // {
+      //   test: /\.(png|jpg|gif|svg)$/,
+      //   loader: 'file-loader',
+      //   options: {
+      //     name: '[path][name].[ext]',
+      //   },
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: './fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.pug',
+    }),
+    new MiniCssExtractPlugin({ filename: './main.css' })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  mode: 'development'
+}
